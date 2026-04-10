a73aa04516c32f741a3be5291de885530e89b79c
adwaithliftoff <269126028+adwaithliftoff@users.noreply.github.com>
2026-04-10 10:28:57 +0530

    Apply guards to assets

--- backend/src/assets/assets.controller.ts
+++ backend/src/assets/assets.controller.ts
@@ -6,11 +6,17 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { AssetsService } from './assets.service';
 import { CreateAssetDto } from './dto/create-asset.dto';
 import { UpdateAssetDto } from './dto/update-asset.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/roles.decorator';
 
+@Roles('ADMIN', 'SUPER_ADMIN')
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('assets')
 export class AssetsController {
   constructor(private readonly assetsService: AssetsService) {}
