ad00b1075bbe0bae614944877a2d73a3ec2cae04
adwaithliftoff <269126028+adwaithliftoff@users.noreply.github.com>
2026-05-06 16:16:22 +0530

    Fix self guard

--- backend/src/auth/guards/self.guard.ts
+++ backend/src/auth/guards/self.guard.ts
@@ -5,12 +5,16 @@ import {
   Injectable,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class SelfGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+  constructor(
+    private reflector: Reflector,
+    private prisma: PrismaService,
+  ) {}
 
-  canActivate(context: ExecutionContext) {
+  async canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest();
     const paramId = Number(request.params.id);
     const user = request.user;
@@ -22,8 +26,12 @@ export class SelfGuard implements CanActivate {
     );
     if (!requiredPermissions) return true;
 
+    const userPermissions = await this.prisma.rolePermission.findMany({
+      where: { role: user.role },
+    });
+    const permissions = userPermissions.map((p) => p.permission);
     const hasPermission = requiredPermissions.every((permission) =>
-      user.permissions?.includes(permission),
+      permissions.includes(permission),
     );
 
     if (!hasPermission) throw new ForbiddenException('Access denied');
