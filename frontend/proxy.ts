5fafbd2f73dff1c04011bfd93d14c8458fda109d
adwaithliftoff <269126028+adwaithliftoff@users.noreply.github.com>
2026-05-28 12:02:27 +0530

    Modify proxy to work based on clerk roles

--- frontend/proxy.ts
+++ frontend/proxy.ts
@@ -1,15 +1,26 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher(["/login(.*)"]);
+const isAdminRoute = createRouteMatcher(["/", "/employees(.*)", "/assets(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect();
+  if (isPublicRoute(req)) return;
+
+  const { isAuthenticated, has, redirectToSignIn } = await auth();
+
+  if (!isAuthenticated) {
+    return redirectToSignIn();
+  }
+  if (isAdminRoute(req) && !has({ role: "org:admin" })) {
+    return NextResponse.redirect(new URL("/tickets", req.url));
   }
 });
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:png|jpg|jpeg|gif|svg|ico|css|js)$).*)",
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+    "/__clerk/(.*)",
   ],
 };
